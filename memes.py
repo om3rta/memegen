55c7a1c6bf4cd8503eb8e322b176d86304993c60
om3rta <spamitycalamity@gmail.com>
2015-10-27 02:12:09 -0400

    Added logic to handle entering different numbers of arguments. Also added a list option that will return a list of all available memes from the meme api

--- memes.py
+++ memes.py
@@ -12,10 +12,18 @@ def url():
     a url. Tests url to make sure a meme is created
     if not, user is notified
     """
+    if len(sys.argv) < 2:
+        print "You must at least input the name of a meme"
+        return False
     raw_meme_name = sys.argv[1]
     cap_meme_name = raw_meme_name.title()
     formatted_meme_text = "+".join(raw_meme_name.split())
     formatted_cap_meme_text = "+".join(cap_meme_name.split())
+    if sys.argv[1] == "list":
+        list_url = urllib2.urlopen("http://apimeme.com/images").read()
+        list_lines = re.sub(r",","\r\n", list_url)
+        print list_lines
+        return False
     if len(sys.argv) >= 3:
         raw_top_text = sys.argv[2]
         formatted_top_text = "+".join(raw_top_text.split())
@@ -28,7 +36,7 @@ def url():
         formatted_bottom_text = ""
     if len(sys.argv) > 4:
         print "You've added too many arguments"
-        return "NULL"
+        return False
     meme_url = "http://apimeme.com/meme?meme=" + formatted_meme_text + "&top=" + formatted_top_text + "&bottom=" + formatted_bottom_text
     test_url = urllib2.urlopen(meme_url).read()
     matches = re.findall('No meme found', test_url);
@@ -42,8 +50,8 @@ def url():
         if len(matches) == 0: 
             return meme_url
         else:
-            print "That meme does not exist"
-            return "NULL"
+            print "That meme does not exist. Try again, or use 'list' as the only argument to show the lsit of available memes"
+            return False
 
 def shorten(address):
     """
@@ -59,7 +67,7 @@ def clipboard_copy(output):
     pyperclip.copy(output)
 
 long_url = url()
-if long_url == "NULL":
+if long_url == False:
     pass
 else:
     short = shorten(long_url)
